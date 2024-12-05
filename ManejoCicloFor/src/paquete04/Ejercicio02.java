76a24aadba3f8b52842e14e5c3e8c314e4528d7f
unknown <aaarias19@utpl.edu.ec>
2024-12-05 11:55:29 -0500

    cambiosejercicio02

--- ManejoCicloFor/src/paquete04/Ejercicio02.java
+++ ManejoCicloFor/src/paquete04/Ejercicio02.java
@@ -11,5 +11,16 @@ package paquete04;
 public class Ejercicio02 {
     public static void main(String[] args) {
         
+        double valorFarenheit= 20;
+        double acumulador= 0;
+        double valorCelcius=0;
+        double operacion;
+        
+        for (int i = 1; i <= 20; i++) {
+            double fahrenheit = 20 + (i * 4);
+            operacion = (5/9* valorFarenheit - 32);
+            acumulador += valorCelcius;
+        }
+        
     }
 }
